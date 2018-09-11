13f4f8c3c195f0eee24bc212241766fc31ed62d1
ELK75 <elijah.kajinic@gmail.com>
2018-09-10 21:25:21 -0400

    clean up graph display

--- src/components/graph.js
+++ src/components/graph.js
@@ -6,13 +6,13 @@ import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
 export default (props) => {
 
   return (
-    <div className="card col-12 col-md-6 col-lg-4">
+    <div className="card col-12 col-md-6 col-lg-4 display-graph">
       <Sparklines height={100} width={160} data={props.data}>
         <SparklinesLine color={"blue"} />
         <SparklinesSpots />
       </Sparklines>
 
-      <h4>{props.text}</h4>
+      <h4 className="display-graph__text">{props.text}</h4>
     </div>
   );
 }
