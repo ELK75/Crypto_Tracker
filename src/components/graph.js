de997a01382bf248bdc7bbf0cff127fa49bfac5e
ELK75 <elijah.kajinic@gmail.com>
2018-09-11 20:04:25 -0400

    first commit

--- src/components/graph.js
+++ src/components/graph.js
@@ -4,9 +4,7 @@ import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
 
 
 export default (props) => {
-
-  console.log(props.color);
-
+  
   return (
     <div className="card col-12 col-md-6 col-lg-4 display-graph">
       <Sparklines style={{background: "#f7f7f7"}} height={100} width={160} data={props.data}>
